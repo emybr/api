8c6b8cae1b87a7b1c0a10d99c6a1c61dc259215b
emybr <emybr82ar@gmail.com>
2023-07-19 22:35:10 -0300

    mongo2

--- src/sessions/sessionConfig.cjs
+++ src/sessions/sessionConfig.cjs
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const sessionConfig = (app) => {
     const store = MongoStore.create({
-        mongoUrl: "mongodb+srv://emybr82ar:92713@cluster0.apsr9qa.mongodb.net/?retryWrites=true&w=majority",
+        mongoUrl: "mongodb+srv://emy82ar:aSvp7zNoOouhsg8r@testcluster0.tjbi9zz.mongodb.net/?retryWrites=true&w=majority",
         mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
         ttl: 60 * 60 * 24 * 7, // 1 week
         collectionName: 'sessions'
